9f946b16cdfd332b847bc095cf45f18705ccd115
Daniel Lefebvre <daniel.lefebvre@intuisix.com>
2019-11-18 09:53:02 +0100

    Essai à conserver

--- src/Form/FilterType.php
+++ src/Form/FilterType.php
@@ -18,8 +18,18 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
+/**
+ * Formulaire permettant de filtrer les relevés affichés.
+ */
 class FilterType extends AbstractType
 {
+    /**
+     * Génère le formulaire.
+     *
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     * @return void
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -33,20 +43,6 @@ class FilterType extends AbstractType
                 'required' => false,
                 'widget' => 'single_text',
             ])
-//            ->add('name')
-/*            ->add('systems', CollectionType::class, [
-                'label' => "Systèmes",
-                'entry_type' => EntityType::class,
-                'entry_options' => [
-                    'class' => System::class,
-                    'choice_label' => 'name',
-                    'placeholder' => "Choisissez un système",
-                    'required' => false,
-                ],
-                'allow_add' => true,
-                'allow_delete' => true,
-                'delete_empty' => true,
-            ]) */
             ->add('systems', EntityType::class, [
                 'choice_label' => 'name',
                 'class' => System::class,
@@ -55,74 +51,28 @@ class FilterType extends AbstractType
                 'multiple' => true,
                 'placeholder' => "Choisissez un système",
                 'required' => false,
-            ])
-/*            ->add('basins', EntityType::class, [
-                'choice_label' => 'name',
-                'class' => Basin::class,
-                'expanded' => true,
-                'label' => "Bassins",
-                'multiple' => true,
-                'placeholder' => "Choisissez un bassin",
-                'query_builder' => function(BasinRepository $br) {
-                    return $br->createQueryBuilder('b')
-                        ->orderBy('b.name', 'ASC');
-                },
-                'required' => false,
-            ])
-            ->add('stations', EntityType::class, [
-                'choice_label' => 'name',
-                'class' => Station::class,
-                'expanded' => true,
-                'label' => "Stations",
-                'multiple' => true,
-                'placeholder' => "Choisissez une station",
-                'query_builder' => function(StationRepository $sr) {
-                    return $sr->createQueryBuilder('s')
-                        ->orderBy('s.name', 'ASC');
-                },
-                'required' => false,
-            ])*/
-        ;
-
-        $formModifier = function($form, $systems = null) {
-            $form->add('basins', EntityType::class, [
-                'choice_label' => 'name',
-                'class' => Basin::class,
-                'multiple' => true,
-                'required' => false,
-                'expanded' => true,
-                'query_builder' => function(BasinRepository $br) use ($systems) {
-                    $qb = $br->createQueryBuilder('b');
-                    if (count($systems) > 0) {
-                        $qb->where(
-                            $qb->expr()->in(
-                                'b.system',
-                                $systems->map(function($system) {
-                                    return $system->getId();
-                                })->getValues()
-                            ));
-                    }
-                    return $qb->orderBy('b.name', 'ASC');
-                }
             ]);
-        };
 
         $builder
             ->addEventListener(
                 FormEvents::PRE_SET_DATA,
-                function (FormEvent $event) use ($formModifier) {
+                function (FormEvent $event) {
                     $data = $event->getData();
-                    $formModifier($event->getForm(), $data->getSystems());
+                    $form = $event->getForm();
+                    $this->addBasinsField($form, $data->getSystems());
+                    $this->addStationsField($form, $data->getBasins());
                 }
             )
             ->get('systems')->addEventListener(
                 FormEvents::POST_SUBMIT,
-                function (FormEvent $event) use ($formModifier) {
-                    $systems = $event->getForm()->getData();
-                    $formModifier($event->getForm()->getParent(), $systems);
+                function (FormEvent $event) {
+                    $form = $event->getForm();
+                    $parent = $form->getParent();
+                    dump($parent);
+                    $this->addBasinsField($parent, $form->getData());
+                    $this->addStationsField($parent, $parent->get('basins')->getData());
                 }
-            )
-        ;
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver)
@@ -130,5 +80,62 @@ class FilterType extends AbstractType
         $resolver->setDefaults([
             'data_class' => Filter::class,
         ]);
+    }
+
+    private function addBasinsField($form, $systems = null) {
+        $form
+            ->add('basins', EntityType::class, [
+                'choice_label' => 'name',
+                'class' => Basin::class,
+                'multiple' => true,
+                'required' => false,
+                'expanded' => true,
+                'query_builder' => function(BasinRepository $br) use ($systems) {
+                    $qb = $br->createQueryBuilder('b');
+                    if (count($systems) == 0) {
+                        $qb->where('b.system is null');
+                    } else {
+                        $qb->where(
+                            $qb->expr()->in(
+                                'b.system',
+                                $systems->map(function($system) {
+                                    return $system->getId();
+                                })->getValues()
+                            )
+                        );
+                    }
+                    return $qb->orderBy('b.name', 'ASC');
+                }
+            ]);
+    }
+
+    private function addStationsField($form, $basins = null) {
+        $form
+            ->add('stations', EntityType::class, [
+                'choice_label' => 'name',
+                'class' => Station::class,
+                'expanded' => true,
+                'label' => "Stations",
+                'multiple' => true,
+                'placeholder' => "Choisissez une station",
+                'query_builder' => function(StationRepository $sr) use ($basins) {
+                    dump($basins);
+                    $qb = $sr->createQueryBuilder('s');
+                    if (count($basins) == 0) {
+                        $qb->where('s.basin is null');
+                    } else {
+/*                        $qb->where(
+                            $qb->expr()->in(
+                                's.basin',
+                                $basins->map(function($basin) {
+                                    return $basin->getId();
+                                })->getValues()
+                            )
+                        );*/
+                    }
+                    return $qb->orderBy('s.name', 'ASC');
+                },
+                'required' => false,
+            ]);
     }
 }
