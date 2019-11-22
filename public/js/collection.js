4a92ea1692880c14d17bf3e283b4e7351fae3256
Daniel Lefebvre <daniel.lefebvre@intuisix.com>
2019-11-22 11:09:57 +0100

    Correction et documentation de la gestion de sous-formulaires

--- public/js/collection.js
+++ public/js/collection.js
@@ -1,31 +1,115 @@
-function createCollectionResponders(blockSelector, additionSelector, deletionSelector, deletionMessage = null) {
+/* Gère les réponses aux utilisateurs sur les boutons d'ajout et de suppression
+des éléments d'une CollectionType de Symfony.
 
-    /* Obtenir le nombre initial de mesures */
-    var itemCount = +$(blockSelector).length;
+@param $blockSelector est l'identifiant de la division contenant les sous-
+formulaires correspondant aux éléments de la collection. Ne pas oublier d'y
+préfixer '#'. Ce paramètre doit référencer un seul élément de document.
 
-    /* Définir l'action des boutons de suppression */
-    function setupDeleteButtons() {
-        /* Eviter plusieurs demandes de confirmation sur un même événement */
-        $(deletionSelector).off("click");
-        /* Supprimer le bloc de formulaire en cas de clic */
-        $(deletionSelector).on("click", function() {
-            if ((null == deletionMessage) || confirm(deletionMessage)) {
+@param $additionSelector est l'identifiant du ou des boutons d'ajout d'élément,
+qui déclencheront l'insertion de sous-formulaires accueillant les nouveaux
+éléments. Il doit désigner des boutons, qui doivent se trouver n'importe où
+dans le document, éventuellement même à l'intérieur de sous-formulaires. Chaque
+sous-formulaire sera créé avec un identifiant comme si Symfony le générait
+lui-même (id="{{id}}" en Twig). Ce paramètre peut être nul pour ne pas gérer
+l'ajout.
+
+@param delectionSelector est l'identifiant du ou des boutons de suppression
+d'élément, qui déclencheront la suppression des sous-formulaire en vue de
+supprimer l'élément qui leur correspond. Il doit désigner des boutons dont la
+cible désigne le sous-formulaire à supprimer (data-target="#{{id}}"" en Twig).
+Ce paramètre peut être nul pour ne pas gérer la suppression.
+
+@param $deletionMessage est le message de la confirmation qui sera demandée à
+l'utilisateur lorsqu'il clique sur le bouton de suppression. Ce paramètre peut
+être nul pour procéder à la suppression sans demander de confirmation.
+*/
+function createCollectionResponders($blockSelector, $additionSelector, $deletionSelector, $deletionMessage = null) {
+
+    /* Symfony a déjà numéroté de façon unique les éléments déjà présents;
+    obtenir le nombre initial d'éléments dans la collection, afin de pouvoir
+    continuer la numérotation en réponse aux ajouts d'éléments demandés par
+    l'utilisateur */
+    var $itemIndex = +$($blockSelector).children().length;
+
+    /* Définit l'action des boutons de suppression d'élément, qui sont inclus
+    dans un ou plusiers sous-formulaire(s) */
+    function setupDeleteButtons($selector)
+    {
+        /* Supprimer le sous-formulaire en cas de clic, après avoir demandé
+        confirmation à l'utilisateur si un message a été spécifié */
+        $($selector).click(function() {
+            if ((null == $deletionMessage) || confirm($deletionMessage)) {
                 $(this.dataset.target).remove();
             }
         });
     }
 
-    /* Définir l'action des boutons d'ajout */
-    function setupAddButtons() {
-        setupDeleteButtons();
-        /* Ajouter un bloc de formulaire en cas de clic */
-        $(additionSelector).click(function() {
-            itemCount++;
-            const prototype = $(blockSelector).data('prototype');
-            $(blockSelector).append(prototype.replace(/__name__/g, itemCount));
-            setupDeleteButtons();
+    /* Retourne le sélecteur d'un sous-formulaire */
+    function getItemSelector($index)
+    {
+        return $blockSelector + '_' + $index;
+    }
+
+    /* Définit l'action des boutons d'ajout d'élément, qui sont inclus dans le
+    formulaire principal et/ou dans un ou plusieurs sous-formulaires */
+    function setupAddButtons($selector) 
+    {
+        /* Ajouter un sous-formulaire en cas de clic */
+        $($selector).click(function() {
+            /* Attribuer un numéro unique à l'élément, en principe dans la
+            continuité des numéros existants; cependant, lorsque le formulaire
+            est affiché une deuxième ou une énième fois après que l'utilisateur
+            ait supprimé des éléments, il se peut que des discontinuités soient
+            présentes dans la numérotation, ou même des inversions; par
+            conséquent, la boucle permet de trouver un numéro qui n'est pas
+            encore attribué */
+            var $itemSelector = getItemSelector($itemIndex);
+            while ($($itemSelector).length) {
+                $itemIndex++;
+                $itemSelector = getItemSelector($itemIndex);
+            }
+            /* Un prototype du sous-formulaire a été créé par Symfony: pour
+            ajouter le sous-formulaire correspondant au nouvel élément, il
+            suffit le dupliquer et d'y remplacer la chaîne __name__ par
+            l'indice du nouvel élément */
+            const prototype = $($blockSelector).data('prototype');
+            $($blockSelector).append(prototype.replace(/__name__/g, $itemIndex));
+            /* Configure les éventuels boutons ajoutés par le sous-formulaire */
+            if (null != $additionSelector) {
+                setupAddButtons($itemSelector + ' ' + $additionSelector);
+            }
+            if (null != $deletionSelector) {
+                setupDeleteButtons($itemSelector + ' ' + $deletionSelector);
+            }
         });
     }
 
-    return setupAddButtons;
+    /* Configure les boutons existants */
+    function setupButtons()
+    {
+        if (null != $additionSelector) {
+            setupAddButtons($additionSelector);
+        }
+        if (null != $deletionSelector) {
+            setupDeleteButtons($deletionSelector);
+        }
+    }
+
+    /* Réaliser quelques vérifications de base et afficher des avertissements
+    sur la console: si des sélecteurs sont spécifiés, au moins une occurrence
+    doit être présente soit dans le formulaire lui-même soit dans le prototype
+    de sous-formulaire */
+    if (1 != $($blockSelector).length) {
+        console.error($blockSelector, 'not found or not unique')
+    }
+    if ((null != $additionSelector) && (
+        (0 == $($additionSelector).length))) {
+        console.error($additionSelector, 'defined but not found')
+    }
+    if ((null != $deletionSelector) && 
+        (0 == $($deletionSelector).length)) {
+        console.error($deletionSelector, 'defined but not found', )
+    }
+
+    return setupButtons;
 }
